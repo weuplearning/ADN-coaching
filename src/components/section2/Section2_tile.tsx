b50d9ab86d5f411b53d2815c4304baf246fd9b31
quentinartaud <quentin.artaud@weuplearning.com>
2023-04-18 18:17:57 +0200

    adding style

--- src/components/section2/Section2_tile.tsx
+++ src/components/section2/Section2_tile.tsx
@@ -7,20 +7,27 @@ interface Section2_tileProps {
 
 const Section2_tile: React.FC<Section2_tileProps> = ({ professor }) => (
     <div className='section2_tile'>
-        <div className="section2_tile-topSection">
-            <p className='section2_Tile-category'>{professor.category}</p>
-            <div className="section2_tile-moreInfos"></div>
-            <img src={professor.imagePath} alt={professor.firstName+' '+professor.lastName} />
+        <div className="section2_tile-content">
+            <div className="section2_tile-topSection">
+                <p className='section2_tile-category'>{professor.category}</p>
+            </div>
+            <div className="section2_tile-middleSection">
+                <img className='section2_tile-img' src={professor.imagePath} alt={professor.firstName+' '+professor.lastName} />
+                <div className="section2_tile-moreInfos"></div>
+                <div className="section2_tile-middleSection-text">
+                    <h3 className='section2_tile-name'>{professor.firstName} {professor.lastName}</h3>
+                    <p className='section2_tile-jobTitle'>{professor.title}</p>
+                </div>
+            </div>
+            <p className='section2_tile-themes'>Thematiques abordées en coaching :</p>
+            <ul>
+                {professor.courseThemes.map(theme => (
+                    <li key={theme}>{theme}</li>
+                ))}
+            </ul>
+            <div className="spacer"></div>
+            <a className='section2_tile-button' href="">reserver un coaching</a>
         </div>
-        <h3 className='section2_Tile-name'>{professor.firstName} {professor.lastName}</h3>
-        <p className='section2_Tile-jobTitle'>{professor.title}</p>
-        <p className='section2_Tile-themes'>Thematiques abordées en coaching :</p>
-        <ul>
-            {professor.courseThemes.map(theme => (
-                <li key={theme}>{theme}</li>
-            ))}
-        </ul>
-        <a className='section2_tile-button' href="">reserver un coaching</a>
     </div>
 )
 
