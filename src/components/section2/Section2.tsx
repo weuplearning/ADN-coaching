8e7176306b9c1f57c74609822949db14c93a88b3
quentinartaud <quentinartaud@gmail.com>
2023-05-23 13:02:25 +0200

    global polishing + filter button now /hide show up categories

--- src/components/section2/Section2.tsx
+++ src/components/section2/Section2.tsx
@@ -8,41 +8,50 @@ interface Section2Props {
 
 const Section2: React.FC<Section2Props> = ({ professors }) => {
 
-    const [selectedCategories, setselectedCategories] = useState<string[] > ([])
+    const [selectedCategories, setselectedCategories] = useState<string[]>([])
+    const [menuOpen, setMenuOpen] = useState(false)
 
     const handleCategoryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        
+
         const category = (event.target.value)
-        
+
         if (event.target.checked) {
-            setselectedCategories(prevSelectedCategories => [...prevSelectedCategories,category])
+            setselectedCategories(prevSelectedCategories => [...prevSelectedCategories, category])
         }
-        else{
+        else {
             setselectedCategories(prevSelectedCategories => prevSelectedCategories.filter(element => element !== category))
         }
     }
-    
+
+    const handleButtonClick = () => {
+        setMenuOpen(!menuOpen)
+    }
+
     return (
         <div className='section2'>
             <div className="section2-top">
                 <h2>Tous nos formateurs</h2>
                 <div className="section2-filter">
-                    <p>filtres</p>
-                    {/* Add checkboxes for each unique course category */}
-                    {[...new Set(professors.map(professor => professor.category))].map(category => (
-                        <label key={category}>
-                            <input type="checkbox" value={category} onChange={handleCategoryChange} />
-                            {category}
-                        </label>
-                    ))}
+                    <div onClick={handleButtonClick} className='section2-filterButton'>filtres</div>
+                    <div className={`section2-filterItems ${menuOpen ? 'section2-filterItems-open' : ''}`}>
+                        {menuOpen &&
+                            // Add checkboxes for each unique course category
+                            [...new Set(professors.map(professor => professor.category))].map(category => (
+                                <label key={category}>
+                                    <input type="checkbox" value={category} onChange={handleCategoryChange} />
+                                    {category}
+                                </label>
+                            ))
+                        }
+                    </div>
                 </div>
             </div>
             <div className="divider section2-divider"></div>
             <div className="professorList">
                 {professors.filter(professor =>
-                    selectedCategories.length ===0 || selectedCategories.includes(professor.category)).map(professor =>(
-                        <Section2_tile key={professor.id} professor={professor}/>
-                ))}
+                    selectedCategories.length === 0 || selectedCategories.includes(professor.category)).map(professor => (
+                        <Section2_tile key={professor.id} professor={professor} />
+                    ))}
             </div>
         </div>
     )
