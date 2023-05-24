1fa6107cfcd5adc5cce285bfb102d58a53134eac
quentinartaud <quentin.artaud@weuplearning.com>
2023-05-24 15:23:28 +0200

    add fetch to get the datas outside of local project + modification on interface file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Section1 from './components/section1/Section1'
 import Section2 from './components/section2/Section2'
 
-import data from './assets/coachsData.json'
+// import data from './assets/coachsData.json'
 import { Professor } from './interfaces/interface';
 
 const App = () => {
 
-    const professors: Professor[] = data.professors
+    // const professors: Professor[] = data.professors
+
+    const [professors, setProfessors] = useState<Professor[]>([])
+
+    useEffect(() => {
+        fetch('https://amazon.koa.qualif.dev/media/microsites/amazon/react_coach/assets/coachsData.json')
+            .then(response => response.json())
+            .then(datas => setProfessors(datas.professors))
+    }, [])
 
     return (
         <div className="app">
             <div className="pageTitleSection">
                 <h1 className='pageTitle'>Rencontrez l'un de nos coachs</h1>
             </div>
-            <Section1 professors={professors}/>
-            <Section2 professors={professors}/>
+            <Section1 professors={professors} />
+            <Section2 professors={professors} />
         </div>
     )
 }
